0e833101637c5fce3a90cc12e15548b46e7b92a5
Bleu Rubin <bleurubin@gmail.com>
2025-11-30 10:40:49 -0500

    org email

--- src/main/java/org/budgetanalyzer/currency/config/OpenApiConfig.java
+++ src/main/java/org/budgetanalyzer/currency/config/OpenApiConfig.java
@@ -18,7 +18,7 @@ import org.budgetanalyzer.service.config.BaseOpenApiConfig;
             title = "Currency Service",
             version = "1.0",
             description = "API documentation for Currency Service resources",
-            contact = @Contact(name = "Bleu Rubin", email = "budgetanalyzer@proton.me"),
+            contact = @Contact(name = "Bleu Rubin", email = "contact@budgetanalyzer.org"),
             license = @License(name = "MIT", url = "https://opensource.org/licenses/MIT")),
     servers = {
       @Server(url = "http://localhost:8080/api", description = "Local environment (via gateway)"),
