de98b48ebac9e2ef11734585ca5e5345d78769ce
Bleu Rubin <bleu@budgetanalyzer.org>
2026-03-12 09:21:43 +0700

    use libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.springframework.boot.gradle.tasks.run.BootRun
 
 // TC 1.21.4 fixes Docker 29.x compatibility (1.21.3 breaks with "client version 1.32 is too old").
 // Spring Boot 3.5.7 manages TC to 1.21.3, so we override it here.
-extra["testcontainers.version"] = "1.21.4"
+extra["testcontainers.version"] = libs.versions.testcontainers.get()
 
 plugins {
     java
@@ -62,7 +62,7 @@ dependencies {
     runtimeOnly(libs.postgresql)
 
     testImplementation(libs.spring.boot.starter.test)
-    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation(libs.spring.security.test)
     testImplementation(libs.spring.boot.testcontainers)
     testImplementation(libs.spring.modulith.starter.test)
     testImplementation(libs.testcontainers)
