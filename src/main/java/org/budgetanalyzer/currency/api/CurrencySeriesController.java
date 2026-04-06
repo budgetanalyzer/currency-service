f4cebf93156c5c21c2cffb2d9b2d9211cabd4857
Bleu Rubin <bleu@budgetanalyzer.org>
2026-04-06 05:18:51 -0700

    remove redundant created status annotation

--- src/main/java/org/budgetanalyzer/currency/api/CurrencySeriesController.java
+++ src/main/java/org/budgetanalyzer/currency/api/CurrencySeriesController.java
@@ -6,7 +6,6 @@ import jakarta.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -137,7 +135,6 @@ public class CurrencySeriesController {
       })
   @PreAuthorize("hasAuthority('currencies:write')")
   @PostMapping(produces = "application/json", consumes = "application/json")
-  @ResponseStatus(HttpStatus.CREATED)
   public ResponseEntity<CurrencySeriesResponse> create(
       @Valid @RequestBody CurrencySeriesCreateRequest request) {
     log.info("Creating currency series for currency code: {}", request.currencyCode());
